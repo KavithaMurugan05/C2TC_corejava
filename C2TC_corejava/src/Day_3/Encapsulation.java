116b363600b488495e2d2c4a9043838581ace6aa
KavithaMurugan05 <kavitha971729@gmail.com>
2025-07-24 09:59:15 +0530

    day3

---
+++ C2TC_corejava/src/Day_3/Encapsulation.java
@@ -0,0 +1,14 @@
+package Day_3;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+					OopsConceptDemo obj = new OopsConceptDemo();
+					obj.setSerialNum(717);
+					obj.setName("Ritvika");
+					obj.setAge(20);
+					System.out.println(obj);
+
+		}
+	}
+
---
+++ C2TC_corejava/src/Day_3/Person.java
@@ -0,0 +1,63 @@
+package Day_3;
+
+public class Person {
+	private String name;
+    private int income;
+    private String gender;
+    private int age;
+    private int tax;
+    
+    public int getTax() {
+        return tax;
+    }
+
+    public void setTax(int tax) {
+        this.tax = tax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", income=" + income + ", gender=" + gender + ", age=" + age + ", tax=" + tax + "]";
+    }
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("Brainy");
+        p.setIncome(500000);
+        p.setGender("Female");
+        p.setAge(21);
+        p.setTax(50000);
+
+        System.out.println(p);
+    }
+}
