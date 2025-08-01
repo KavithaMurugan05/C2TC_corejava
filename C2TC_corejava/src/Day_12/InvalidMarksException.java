fb8e37406f34e853a2f8c1c5fa1371e85e044a66
KavithaMurugan05 <kavitha971729@gmail.com>
2025-08-01 22:30:52 +0530

    day12

---
+++ C2TC_corejava/src/Day_12/InvalidMarksException.java
@@ -0,0 +1,11 @@
+package Day_12;
+
+public class InvalidMarksException extends Exception{
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
